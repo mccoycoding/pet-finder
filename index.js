6b9fd25dba10c5f9bfb90f36812f6cf19aa3ebbf
McCoy <mccoycoding@gmail.com>
2023-08-14 07:50:13 -0700

    stuck

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const app = express();
 
 const PORT = 8080;
 
+app.use(express.static('public'))
+
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + '/index.html')
 });
 
 // hello world route
@@ -21,35 +23,43 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
-});
-
-// get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
-    // get the owner from the request
-
-
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
-
-    // send the pet as a response
-
+    res.send(pets)
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
+    // get the name from the route parameter
+    const { name } = req.params;
 
-
+    console.log(name)
+    
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
-
+    console.log(pet)
     // send the pet as a response
-
+    if (pet) {
+        res.send(pet);
+    } else {
+        res.status(404).send('Pet not found.');
+    }
 });
 
+// get pet by owner with query string
+app.get('/api/v1/pets/:owner', (req, res) => {
+    // get the owner from the request
+    const {owner} = req.query
+
+    // find the pet in the pets array
+    const pet = pets.filter(pet => pet.owner === owner);
+
+    // send the pet as a response
+    res.send(pet)
+});
+
+
+
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
+    console.log(`Server is listening at http://localhost:${PORT}`);
 });
 
 module.exports = app;
